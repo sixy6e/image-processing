96f49fe3c2644b6cd5506f7f54117c8378db0fb5
Josh Sixsmith <josh.sixsmith@gmail.com>
2014-08-20 21:24:58 +1000

    Initial imports.

---
+++ src/enhancement/contrast.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+def linear_percent():
+"""
+
+"""
+
+def hist_equalisation():
+"""
+
+"""
+
+def square_root():
+"""
+
+"""
+
+def gauss():
+"""
+
+"""
+
+def hist_match():
+"""
+
+"""
+
