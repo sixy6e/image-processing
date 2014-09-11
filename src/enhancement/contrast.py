9318399e0e113098aa5156141eacea1ba510f23b
Josh Sixsmith <josh.sixsmith@gmail.com>
2014-09-12 09:02:22 +1000

    Started fleshing out some functions.

--- src/enhancement/contrast.py
+++ src/enhancement/contrast.py
@@ -1,27 +1,75 @@
 #! /usr/bin/env python
+import numpy
+from IDL_functions import histogram
+from IDL_functions import hist_equal
+from IDL_functions import bytscl
 
-def linear_percent():
-"""
+def data_convert(val, b):
+    """
+    
+    """
+    instr = str(val)
+    return {
+        'int8' : numpy.int8(b),
+        'uint8' : numpy.uint8(b),
+        'int16' : numpy.int16(b),
+        'uint16' : numpy.uint16(b),
+        'int32' : numpy.int32(b),
+        'uint32' : numpy.uint32(b),
+        'int64' : numpy.int64(b),
+        'uint64' : numpy.uint64(b),
+        'int' : numpy.int64(b),
+        'float32' : numpy.float32(b),
+        'float64' : numpy.float64(b),
+        }.get(instr, 'Error')
 
-"""
+def calculate_binsize(array, nbins=256):
+    """
+    
+    """
+    # Calculate min and max. We always check for NaN's
+    Max = numpy.nanmax(array)
+    Min = numpy.nanmin(array)
 
-def hist_equalisation():
-"""
+    # Convert min/max to array datatype
+    Min = data_convert(array.dtype.name, Min)
+    Max = data_convert(array.dtype.name, Max)
 
-"""
+    # ENVI appears to ceil the result
+    binsize = numpy.ceil((Max - Min) / (nbins - 1))
+
+    return binsize
+
+def linear_percent(array, percent=2, nbins=256):
+    """
+    
+    """
+    binsize = calculate_binsize(array, nbins=nbins)
+    stretch = hist_equal(array, Binsize=binsize, Percent=Percent)
+
+    return stretch
+
+def equalisation(array, nbins=256):
+    """
+    
+    """
+    binsize = calculate_binsize(array, nbins=nbins)
+    stretch = hist_equal(array, Binsize=binsize)
+
+    return stretch
 
 def square_root():
-"""
-
-"""
+    """
+    
+    """
 
 def gauss():
-"""
-
-"""
+    """
+    
+    """
 
 def hist_match():
-"""
-
-"""
+    """
+    
+    """
 
