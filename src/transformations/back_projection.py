9ffa298c480202f136b2a9d4ec93b909422a1ce2
Josh Sixsmith <josh.sixsmith@gmail.com>
2014-09-27 23:18:21 +1000

    Initial outlay of function.

---
+++ src/transformations/back_projection.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from IDL_functions import histogram
+
+def histogram_backprojection(array, roi, nbins=256):
+    """
+    
+    """
+
+    h = histogram(roi.ravel(), nbins=nbins, omin='omin', omax='omax', locations='loc')
+
+    hist = h['histogram']
+    omin = h['omin']
+    omax = h['omax']
+    loc = h['loc']
+
+    binsz = loc[1] - loc[0]
+
+    total_h = numpy.sum(hist, dtype='float')
+
+    # Normalise the histogram
+    norm_h = hist / total_h
+
+    # Produce the probability image
+    # i.e. index into the pdf via the bin location for every pixel
