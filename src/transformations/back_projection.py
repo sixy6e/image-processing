8711462f1a3f040d90b70e4d872eee986d872aba
Josh Sixsmith <josh.sixsmith@gmail.com>
2015-07-06 22:26:57 +1000

    Added comments.

--- src/transformations/back_projection.py
+++ src/transformations/back_projection.py
@@ -6,7 +6,8 @@ def histogram_backprojection(array, roi, nbins=256):
     """
     
     """
-
+    # the histogram of the full image should be done first
+    # so as to get the full dynamic range
     h = histogram(roi, nbins=nbins, omin='omin', omax='omax',
                   locations='loc')
 
@@ -21,6 +22,7 @@ def histogram_backprojection(array, roi, nbins=256):
 
     # Normalise the histogram
     norm_h = hist / total_h
+    # Or use the pdf??? hist / max(hist) ??
 
     # Produce the probability image
     # i.e. index into the pdf via the bin location for every pixel
