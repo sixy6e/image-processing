20e1ace88899755e8fc660f08512d8d861734e8c
Josh Sixsmith <josh.sixsmith@gmail.com>
2015-07-06 22:52:05 +1000

    Minor changes to the build script.

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@
 from distutils.core import setup
 
 setup(name='image_processing',
-      version = '0.2',
+      version = '0.3',
       package_dir = {'image_processing' : 'src'},
-      packages = ['image_processing','image_processing.segmentation','image_processing.terrain','image_processing.thresholding'],
+      #packages = ['image_processing','image_processing.segmentation','image_processing.terrain','image_processing.thresholding'],
+      packages=['image_processing'],
       author = 'Josh Sixsmith',
       author_email = 'josh.sixsmith@gmail.com',
       maintainer = 'Josh Sixsmith',
