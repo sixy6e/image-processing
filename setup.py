8ac71b7141d8f833058c4edfdd71318dc4ae322b
Josh Sixsmith <josh.sixsmith@gmail.com>
2015-07-06 23:18:02 +1000

    Corrected line lengths to 80 char limit.

--- setup.py
+++ setup.py
@@ -3,15 +3,14 @@
 from distutils.core import setup
 
 setup(name='image_processing',
-      version = '0.3',
-      package_dir = {'image_processing' : 'src'},
-      #packages = ['image_processing','image_processing.segmentation','image_processing.terrain','image_processing.thresholding'],
+      version='0.3',
+      package_dir={'image_processing' : 'src'},
       packages=['image_processing'],
-      author = 'Josh Sixsmith',
-      author_email = 'josh.sixsmith@gmail.com',
-      maintainer = 'Josh Sixsmith',
-      description = 'Collection of general image processing routines.',
-      long_description = 'Collections include routines for automatic thresholding, segmentation, rasterisation and terrain modelling.',
-      license = 'BSD',
-     )
-
+      author='Josh Sixsmith',
+      author_email='josh.sixsmith@gmail.com',
+      maintainer='Josh Sixsmith',
+      description='Collection of general image processing routines.',
+      long_description=('Collections include routines for automatic '
+                        'thresholding, segmentation, rasterisation and '
+                        'terrain modelling.'),
+      license='BSD')
