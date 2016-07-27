da06f2f934fc66cabf3271c15b17223b770f158c
Josh Sixsmith <josh.sixsmith@gmail.com>
2016-07-27 21:24:59 +1000

    Bump version in prep for release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='image_processing',
-      version='0.6.0',
+      version='0.6.1',
       package_dir={'image_processing' : 'src'},
       packages=['image_processing'],
       author='Josh Sixsmith',
